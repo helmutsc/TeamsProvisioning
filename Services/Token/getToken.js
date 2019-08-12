84996ac66f345a7d228631554cb8b4d2d705f4bf
helmutsc <53891342+helmutsc@users.noreply.github.com>
2019-08-12 11:44:31 +0200

    debugging

--- Services/Token/getToken.js
+++ Services/Token/getToken.js
@@ -7,10 +7,18 @@ module.exports = function getToken(context) {
 
   return new Promise((resolve, reject) => {
 
+    var txthlp = "";
     // Get the ADAL client
     const authContext = new adal.AuthenticationContext(
       settings().AUTH_URL + settings().TENANT);
-      
+    
+    txthlp=settings().AUTH_URL;
+    context.log(`AUTH_URL ${txthlp}`);
+    txthlp=settings().TENANT;
+    context.log(`TENANT ${txthlp}`);
+    txthlp=settings().CLIENT_SECRET;
+    context.log(`CLIENT_SECRET ${txthlp}`);
+    
     authContext.acquireTokenWithClientCredentials(
       settings().GRAPH_URL, settings().CLIENT_ID, settings().CLIENT_SECRET, 
       (err, tokenRes) => {
